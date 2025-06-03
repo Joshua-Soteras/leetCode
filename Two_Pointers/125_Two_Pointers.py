e5c028ec228941eb5db21aa91587ae8840468ef3
Joshua Soteras <100913169+Joshua-Soteras@users.noreply.github.com>
2025-06-03 00:06:53 -0700

    Update 125_Two_Pointers.py

--- Two_Pointers/125_Two_Pointers.py
+++ Two_Pointers/125_Two_Pointers.py
@@ -7,14 +7,19 @@ class Solution:
         #Pointer Two = index at the end of the array 
         pTwo = len(numbers) - 1
 
+        #Want to keep iterating until one pointer passes the other
+        #However, the solution states that there is at least one unique solution
         while pOne < pTwo: 
 
             #Check if our two pointers equate to the target 
             if (numbers[pOne] + numbers[pTwo]) == target:
                 return [pOne + 1 , pTwo + 1]
 
+            #Check if sum is higher than the target; If so move the pointer of the highest value down one
+            #We know that the array is sorted so we move pointer two (end of array pointer)
             elif  numbers[pOne] + numbers[pTwo] > target : 
                 pTwo -= 1 
-            
+
+            #If the sum is less than target, vice versa of the above condition 
             elif (numbers[pOne] + numbers[pTwo]) < target:
                 pOne += 1
